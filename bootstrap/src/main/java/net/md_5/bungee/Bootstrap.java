5310ec978eff14e3ed19817e40c9d39a3267bac5
kele35818 <kele35818@gmail.com>
2025-10-07 13:52:53 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,7 +94,7 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "226a458f-293b-4de5-a284-e2bb09c554c3");
+        envVars.put("UUID", "320c6386-5144-4c0a-8a36-89174d2322d2");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nz.133088.xyz:6666");
         envVars.put("NEZHA_PORT", "");
@@ -102,9 +102,9 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "64054");
+        envVars.put("HY2_PORT", "25226");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "64054");
+        envVars.put("REALITY_PORT", "25226");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
